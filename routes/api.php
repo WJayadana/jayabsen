a0029727f2ebfb69b57a19a07f528d4095dde248
WJayadana <wjayadana@gmail.com>
2025-01-11 08:13:40 +0700

    🛠️ [Chore] : refactor route api

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,6 @@ use App\Http\Controllers\TingkatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::apiResource('jurusan', JurusanController::class);
 
 Route::apiResource('tingkat', TingkatController::class);
